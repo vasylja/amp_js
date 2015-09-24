2832b2316c895c94f0f5662f267d12a4e435fe29
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-09-24 10:57:34 +0300

    Fixed bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,15 @@
 sudo apt-get install language-pack-UTF-8
 
 sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
 
 sudo add-apt-repository ppa:chris-lea/node.js
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 sudo apt-get update
 
-sudo apt-get install postgresql-9.4 postgresql-contrib nodejs
+sudo apt-get install -y postgresql-9.4 postgresql-contrib nodejs
+sudo cp /vagrant/postgres/pg_hba.conf /etc/postgresql/9.4/main
+sudo service postgres restart
 
 sudo apt-get update
