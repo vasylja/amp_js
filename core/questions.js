2bfb278fa943eb5ac2e18b166ac24c4bcda4d2a0
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-10-02 11:00:57 +0300

    Added initial questions dal

---
+++ core/questions.js
@@ -0,0 +1,35 @@
+var db = require('../db');
+//var Promise = require('bluebird');
+
+module.exports = {
+	create: create,
+	findAll: findAll,
+	find: find,
+	update: update,
+	remove: remove
+};
+
+// params = { name: 'Test name' }
+function create (params) {
+	return db('questions').insert(params); // []
+}
+
+function findAll () {
+	//return db.select('*').from('questions');
+	//return db.select( ['id', 'name'] ).from('questions');
+	return db('questions');
+}
+
+function find (id) {
+	//return db('questions').where('id', id); // [ { id: 1} ]
+	//return db('questions').where('id', id).first(); // { id: 1 }
+	return db('questions').where({ id: id }).first(); // { id: 1 }
+}
+
+function update (id, question) {
+	return db('questions').update(question).where({ id: id });
+}
+
+function remove (id) {
+	return db('questions').del().where({ id: id });
+}
