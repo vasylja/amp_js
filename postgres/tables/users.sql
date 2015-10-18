931367e5c49577f8877ec6448a4b27a109e6b7d1
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-10-18 23:17:37 +0300

    AMP-36 Added users email constraint and hint comments

--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -1,6 +1,13 @@
+-- more on create tables in database and constraints
+-- here http://www.postgresql.org/docs/9.4/static/sql-createtable.html
 create table users (
 	id bigserial primary key,
 
 	name text,
-	email text
+	-- not null constraint on users table
+	email text not null,
+
+	-- unique email constraint
+	constraint unique_users_email unique (email)
+
 );
