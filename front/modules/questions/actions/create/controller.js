db0c567a6347650c5d7864674eb6e107273e02bb
vladyslav <va_lysenko@ukr.net>
2015-12-03 16:59:54 +0200

    Deletion of Choices while Questions Creating

--- front/modules/questions/actions/create/controller.js
+++ front/modules/questions/actions/create/controller.js
@@ -8,6 +8,15 @@ angular.module('app').controller('CreateQuestionController', [
         $scope.addChoice = function() {
             $scope.question.choices.push({});
         };
+        $scope.removeChoice = function(choice) {
+            $scope.question.choices.forEach(function(item, i, arr) {
+                console.log(item);
+                console.log(choice);
+               if(item===choice) {
+                   arr.splice(i, 1);
+               }
+            });
+        };
         $scope.save = function(question) {
             CreateQuestionService.save(question);
         }
