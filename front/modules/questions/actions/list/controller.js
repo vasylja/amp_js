df004f86df05c56302156c7ea77185f7498766cd
andrewvakhniuk <vah.a.val@gmail.com>
2015-12-06 20:14:05 +0200

    links on Question list page

--- front/modules/questions/actions/list/controller.js
+++ front/modules/questions/actions/list/controller.js
@@ -3,7 +3,6 @@ angular.module('app').controller('QuestionsListController', [
     function ($scope, $routeParams, QuestionsListService) {
         $scope.editPath = '#/questions/edit';
         $scope.answerPath= '#/questions/answer';
-        $scope.path = '#/questions/edit'
         $scope.pathStats = '#/questions/statistics'
         $scope.questions = QuestionsListService.query();
     }
