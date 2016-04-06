342fd75812a8363c7527f3f0b84313a3333bf3ec
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2016-04-06 20:18:25 +0300

    Added initial frontend interceptor for authentication

---
+++ front/authentication.js
@@ -0,0 +1,14 @@
+angular.module('app.authentication').factory('authenticationInterceptor', [
+	function () {
+		return {
+			// for more read about angular http interceptors 
+			request: function (config) {
+				// https://www.npmjs.com/package/store
+				// use it to get token from localStorage
+				//var token = store.get('token');
+				var token = 'lkasjdflkjaf';
+				config.headers.Authorization = 'Bearer ' + token;
+			}
+		};
+	}
+])
--- front/main.js
+++ front/main.js
@@ -1,2 +1,7 @@
-angular.module('app', ['ngRoute', 'ngResource']).run();
+angular.module('app', [
+	'ngRoute', 'ngResource', 'app.authentication'
+]).config(['$httpProvider', function ($httpProvider) {
+	// interceptors  README - https://docs.angularjs.org/api/ng/service/$http
+	$httpProvider.interceptors.push('authenticationInterceptor');
+}]).run();
 
